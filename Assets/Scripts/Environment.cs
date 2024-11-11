ab851db4a62b3b9ca0c90779839541ff2c80dc25
Nekcilo <132229704+Nekcilo@users.noreply.github.com>
2024-11-11 21:00:10 +0000

    PlayerMovement + Text
    Implemented the starts of player movement and got the text to display on the screen corresponding to the 'block' that had been rolled

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Environment : MonoBehaviour
 {
-    string[] blocks = {"Pavement", "Stairs", "Road", "Bollard", "TrafficLights"};
+    public TMP_Text Textblocks;
 
+    string[] blocks = {"Pavement", "Stairs", "Road", "Bollard", "TrafficLights"};
+    //Pavement = 0, Stairs = 1, Road = 2, Bollard = 3, TrafficLights = 4
+
+    int num = 0;
 
     void BlockSpawn()
     {
-        int num = Random.Range(0, blocks.Length);
-        Debug.Log(blocks[num]);
-
-        //if (num == 0)
-        //{
-            //Then add the if statements for the blocks here
-        //}
+        //Currently: Prints Pavement first (correct), then prints only 1 option after no matter how many times its 'rerolled' (incorrect)
+        if (num == 0) //Pavement
+        {
+            Debug.Log(blocks[num]);
+            Textblocks.text = blocks[num];
+            num = Random.Range(0, blocks.Length);
+            //Then Pavement, Stairs, Road and TrafficLights can spawn
+        }
+        if (num == 1) //Stairs
+        {
+            Debug.Log(blocks[num]);
+            Textblocks.text = blocks[num];
+            //Then Pavement and Stairs can spawn
+        }
+        if (num == 2) //Road
+        {
+            Debug.Log(blocks[num]);
+            Textblocks.text = blocks[num];
+            //Then Pavement, Road, Bollard and TrafflicLights can spawn
+        }
+        if(num == 3) //Bollard
+        {
+            Debug.Log(blocks[num]);
+            Textblocks.text = blocks[num];
+            //Then ONLY Road can spawn
+        }
+        if (num == 4) //Traffic Lights
+        {
+            Debug.Log(blocks[num]);
+            Textblocks.text = blocks[num];
+            //Then ONLY Road can spawn
+        }
     }
 
     // Start is called before the first frame update
