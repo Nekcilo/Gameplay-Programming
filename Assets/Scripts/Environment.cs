e121d2fc9f10010460034b5053649b80f47774f4
Em <132229704+Nekcilo@users.noreply.github.com>
2024-11-12 11:46:45 +0000

    Environment Update
    Currently Broken and will freeze unity

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -8,44 +8,83 @@ public class Environment : MonoBehaviour
     public TMP_Text Textblocks;
 
     string[] blocks = {"Pavement", "Stairs", "Road", "Bollard", "TrafficLights"};
+    List<int> blocks2 = new List<int>() {0, };
+    //int[] blocks2 = {0,};
     //Pavement = 0, Stairs = 1, Road = 2, Bollard = 3, TrafficLights = 4
 
     int num = 0;
+    int rounds = 20;
+    int i;
+
+    void debug()
+    {
+        Debug.Log(blocks[num]);
+        Textblocks.text = blocks[num];
+        num = Random.Range(0, blocks.Length);
+        blocks2.Add(num);
+        i++;
+        Debug.Log(i);
+    }
 
     void BlockSpawn()
     {
-        //Currently: Prints Pavement first (correct), then prints only 1 option after no matter how many times its 'rerolled' (incorrect)
-        if (num == 0) //Pavement
+        for (i = 0; i < rounds;) //C# alt to 'for i in range'
         {
-            Debug.Log(blocks[num]);
-            Textblocks.text = blocks[num];
-            num = Random.Range(0, blocks.Length);
-            //Then Pavement, Stairs, Road and TrafficLights can spawn
+            if (num == 0 && blocks2[i] == 0) //Checking if 'i' is equal to 0 (Pavement)
+            {
+                debug();
+                Debug.Log("Round 1");
+
+                num = Random.Range(0, blocks.Length);
+
+                if (num == 0 && blocks2[i] == 0) //Checking if 'i' is equal to 0 (Pavement)
+                {
+                    debug();
+                    Debug.Log("Round 2");
+                }
+                if (num == 1 && blocks2[i] == 1) //Stairs
+                {
+                    debug();
+                    Debug.Log("Round 2");
+                }
+                if (num == 2 && blocks2[i] == 2) //Road
+                {
+                    debug();
+                    Debug.Log("Round 2");
+                }
+                if (num == 4 && blocks2[i] == 4) //Traffic Lights
+                {
+                    debug();
+                    Debug.Log("Round 2");
+                }
+                //Then Pavement, Stairs, Road and TrafficLights can spawn
+            }
+            if (num == 1 && blocks2[i] == 1) //Stairs
+            {
+                debug();
+                Debug.Log("Round 1");
+                //Then Pavement and Stairs can spawn
+            }
+            if (num == 2 && blocks2[i] == 2) //Road
+            {
+                debug();
+                Debug.Log("Round 1");
+                //Then Pavement, Road, Bollard and TrafflicLights can spawn
+            }
+            if (num == 3 && blocks2[i] == 3) //Bollard
+            {
+                debug();
+                Debug.Log("Round 1");
+                //Then ONLY Road can spawn
+            }
+            if (num == 4 && blocks2[i] == 4) //Traffic Lights
+            {
+                debug();
+                Debug.Log("Round 1");
+                //Then ONLY Road can spawn
+            }
         }
-        if (num == 1) //Stairs
-        {
-            Debug.Log(blocks[num]);
-            Textblocks.text = blocks[num];
-            //Then Pavement and Stairs can spawn
-        }
-        if (num == 2) //Road
-        {
-            Debug.Log(blocks[num]);
-            Textblocks.text = blocks[num];
-            //Then Pavement, Road, Bollard and TrafflicLights can spawn
-        }
-        if(num == 3) //Bollard
-        {
-            Debug.Log(blocks[num]);
-            Textblocks.text = blocks[num];
-            //Then ONLY Road can spawn
-        }
-        if (num == 4) //Traffic Lights
-        {
-            Debug.Log(blocks[num]);
-            Textblocks.text = blocks[num];
-            //Then ONLY Road can spawn
-        }
+        
     }
 
     // Start is called before the first frame update
