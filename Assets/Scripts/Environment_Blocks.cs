10d716a351d2f4e27dfea33822ccb56d22c79ba8
Nekcilo <132229704+Nekcilo@users.noreply.github.com>
2024-11-25 10:01:27 +0000

    Added stairs + adjusted spawning positions

--- Assets/Scripts/Environment_Blocks.cs
+++ Assets/Scripts/Environment_Blocks.cs
@@ -6,53 +6,36 @@ using Random = UnityEngine.Random;
 
 public class Environment_Blocks : MonoBehaviour
 {
-    //inspector prefab references
+    //Prefab References
     [SerializeField] private GameObject Pavement_Prefab;
     [SerializeField] private GameObject Road_Prefab;
     [SerializeField] private GameObject Stair_Prefab;
     [SerializeField] private GameObject Bollard_Prefab;
     [SerializeField] private GameObject TrafficLight_Prefab;
 
-    //BoundingBox Info
+    //BoundingBox References
     [SerializeField] private GameObject BoundingBox;
     [SerializeField] private GameObject BB_Left;
     [SerializeField] private GameObject BB_Right;
-    float BB_X = 18f;
-    float BB_Y = 10f;
-    int StairsCount = 0;
     private Vector3 scaleChange, positionChange;
 
-    //to keep track of the number + meaning
-    //string[] Type = {"Pavement", "Road", "Bollard", "TrafficLights"}; //stairs not included
-    //List<int> Spawned = new List<int>() {0,};
-
+    //Lists to track which blocks can be spawned
     List<string> PavementList = new List<string>() {"Pavement", "Road", "Stairs"};
-    List<string> RoadList = new List<string>() {"Pavement", "Bollard", "TrafficLights"};
+    List<string> RoadList = new List<string>() {"Pavement", "Bollard", "TrafficLights", "Stairs"};
     List<string> BollardList = new List<string>() {"Road"};
     List<string> TrafficLightList = new List<string>() {"Road"};
     List<string> StairsList = new List<string>() {"Pavement"};
 
-    //standard variables
-    float X_Position = 18f;
-    float Y_Position = 0f;
+    //Standard Variables
+    float X_Constant = 18f;
+    float Y_Constant = 2.24f;
+    int StairsCount = 0;
     int block_num = 0;
     public int rounds = 20;
 
+    //Makes the 'first' block spawned always be Pavement
     string Previous = "Pavement";
     string Spawn_Choice;
-    
-    void Randomize(int i)
-    {
-      //block_num = Random.Range(0, Type.Length);
-      //Spawned.Add(block_num);
-      
-      /*string result = "List contents: "; //printing Spawned contents
-      foreach (var item in Spawned)
-      {
-        result += item.ToString() + " ";
-      }
-      Debug.Log(result);*/
-    }
     
     public void Spawn()
     {
@@ -63,12 +46,12 @@ public class Environment_Blocks : MonoBehaviour
         switch(Previous)
         {
           case "Pavement":
-          block_num = Random.Range(0, 2); //not including stairs
+          block_num = Random.Range(0, 3);
           Spawn_Choice = PavementList[block_num];
           break;
 
           case "Road":
-          block_num = Random.Range(0, 3);
+          block_num = Random.Range(0, 4); //includes stairs for testing
           Spawn_Choice = RoadList[block_num];
           break;
 
@@ -93,157 +76,60 @@ public class Environment_Blocks : MonoBehaviour
         switch(Spawn_Choice)
         {
           case "Pavement":
-          Instantiate(Pavement_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(Pavement_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn Pavement");
           break;
 
           case "Road":
-          Instantiate(Road_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(Road_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn Road");
           break;
 
           case "Bollard":
-          Instantiate(Bollard_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(Bollard_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn Bollard");
           break;
 
           case "TrafficLights":
-          Instantiate(TrafficLight_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(TrafficLight_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn Traffic Light");
           break;
 
           case "Stairs":
-          Instantiate(Stair_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(Stair_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn Stairs");
           StairsCount++;
           break;
 
           default:
-          Instantiate(Pavement_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
+          Instantiate(Pavement_Prefab, new Vector3(i * X_Constant, StairsCount * Y_Constant, 0), Quaternion.identity);
           Debug.Log("Spawn DEFAULT Pavement");
           break;
         }
 
         Previous = Spawn_Choice;
-
-
-        //OLD CODE
-
-        /*if (Spawned[i] == 0) //pavement
-        {
-	        block_num = Random.Range(0, 1);
-
-          switch(block_num)
-          {
-            default:
-              Instantiate(Pavement_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Pavement");
-            break;
-
-            case 1:
-              Instantiate(Road_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Road");
-            break;
-          }
-        }
-
-        if (Spawned[i] == 1) //road
-        {
-	        block_num = Random.Range(0, 2);
-
-          switch(block_num)
-          {
-            default: //Pavement
-              Instantiate(Pavement_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Pavement");
-            break;
-
-            case 1: //Bollard
-              Instantiate(Bollard_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Bollard");
-            break;
-
-            case 2: //Traffic Lights
-              Instantiate(TrafficLight_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Traffic Light");
-            break;
-
-            case 3: //road
-            Instantiate(Road_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-            Debug.Log("Spawn Road");
-            break;
-          }
-        }
-
-        if (Spawned[i] == 2) //bollard
-        {
-          block_num = Random.Range(0, 1);
-
-          switch(block_num)
-          {
-            default: //road
-            Instantiate(Road_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-            Debug.Log("Spawn Road");
-            break;
-
-            case 1: //Traffic Lights
-              Instantiate(TrafficLight_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Traffic Light");
-            break;
-
-            case 2: //Bollard
-              Instantiate(Bollard_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Bollard");
-            break;
-          }
-        }
-
-        if (Spawned[i] == 3) //traffic light
-        {
-          block_num = Random.Range(0, 2);
-
-          switch(block_num)
-          {
-            default: //road
-            Instantiate(Road_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-            Debug.Log("Spawn Road");
-            break;
-
-            case 1: //Bollard
-              Instantiate(Bollard_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Bollard");
-            break;
-
-            case 2: //Traffic Lights
-              Instantiate(TrafficLight_Prefab, new Vector3(i * X_Position, Y_Position, 0), Quaternion.identity);
-              Debug.Log("Spawn Traffic Light");
-            break;
-
-          }
-        }*/
-        //Randomize(i);
         i++;
       }
       //Main BoundingBox, To stop the camera
       //Scales horizontally with the level blocks, scales vertically from the amount of stairs that spawn
       //Moves Horizontally to be centred with the level
-        scaleChange = new Vector3(((rounds - 1) * BB_X), (StairsCount * BB_Y), 0f);
-        positionChange = new Vector3(((rounds - 1) * (BB_X / 2)), (StairsCount * (BB_Y / 2)), 0f); //has to be divided by 2 to only scale it on the right/top respectively
+        scaleChange = new Vector3(((rounds - 1) * X_Constant), (StairsCount * Y_Constant), 0f);
+        positionChange = new Vector3(((rounds - 1) * (X_Constant / 2)), (StairsCount * (Y_Constant / 2)), 0f); //has to be divided by 2 to only scale it on the right/top respectively
         BoundingBox.transform.localScale += scaleChange;
         BoundingBox.transform.position += positionChange;
 
       //Left bounding box, to stop the player from falling off (left)
       //Scales vertically from the amount of stairs that spawn
-        scaleChange = new Vector3(0f, (StairsCount * BB_Y), 0f);
-        positionChange = new Vector3(0f , (StairsCount * (BB_Y / 2)), 0f);
+        scaleChange = new Vector3(0f, (StairsCount * Y_Constant), 0f);
+        positionChange = new Vector3(0f , (StairsCount * (Y_Constant / 2)), 0f);
         BB_Left.transform.localScale += scaleChange;
         BB_Left.transform.position += positionChange;
 
       //Right bounding box, to stop the player from falling off (right)
       //Scales vertically from the amount of stairs that spawn
       //Moves horizontally to be at the end of the level
-        scaleChange = new Vector3(0f, (StairsCount * BB_Y), 0f);
-        positionChange = new Vector3(((rounds * BB_X) + 2), (StairsCount * (BB_Y / 2)), 0f);
+        scaleChange = new Vector3(0f, (StairsCount * Y_Constant), 0f);
+        positionChange = new Vector3(((rounds * X_Constant) + 2), (StairsCount * (Y_Constant / 2)), 0f);
         BB_Right.transform.localScale += scaleChange;
         BB_Right.transform.position += positionChange;
     }
