115db28acf2b812469b9d0111edae6d863ed25f7
Nekcilo <132229704+Nekcilo@users.noreply.github.com>
2024-11-25 17:12:39 +0000

    Fixed Cinemachine Camera + Adjusted Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    [SerializeField] Rigidbody2D Body;
+    public float moveSpeed;
+    private float moveHorizontal;
+    private bool left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal"); 
+        if (Input.GetAxisRaw("Horizontal") > -1.5 && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Debug.Log("Left");
+            moveSpeed = 1;
+        } 
+        else
+        {
+            moveSpeed = 15;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(Body.velocity.x < 5 && Body.velocity.x > -5)
+        {
+            Body.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+    }
+}
