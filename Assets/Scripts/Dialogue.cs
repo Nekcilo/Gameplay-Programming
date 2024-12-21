465f4a19aeb4397e41442ea92cc26d6129217b7a
Em <132229704+Nekcilo@users.noreply.github.com>
2024-12-21 17:23:26 +0000

    added Wait and Go commands for all blocks
    except for Traffic Lights

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -21,6 +21,11 @@ public class Dialogue : MonoBehaviour
     bool Trigger = false;
     float Secs;
 
+    bool PavementBool = false;
+    bool RoadBool = false;
+    bool TLBool = false;
+    bool StairsBool = false;
+
     void Awake()
     {
 
@@ -38,11 +43,30 @@ public class Dialogue : MonoBehaviour
     {
         if (Trigger == true)
         {
-            Secs -= Time.deltaTime;
             DebugText.SetText((System.Math.Truncate(Secs)).ToString());
+            Secs -= Time.deltaTime;
             if (Secs <= 0)
             {
                 Trigger = false;
+                CommandText.color = new Color32(255, 255, 255, 255);
+                CommandText.outlineWidth = 0.2f;
+                CommandText.outlineColor = new Color32(0, 0, 0, 255);
+                if (PavementBool == true)
+                {
+                    CommandText.SetText("Go"); //Sets text to go once timer is finished, if pavementbool is true
+                }
+                if (RoadBool == true)
+                {
+                   CommandText.SetText("Cross"); //Sets text to cross once timer is finished, if roadbool is true
+                }
+                /*if (TLBool == true)
+                {
+                   CommandText.SetText("Cross"); //Sets text to cross once timer is finished, if tlbool is true
+                }*/
+                if (StairsBool == true)
+                {
+                   CommandText.SetText("Stairs"); //Sets text to stairs once timer is finished, if stairsbool is true 
+                }
             }
         }
     }
@@ -51,7 +75,12 @@ public class Dialogue : MonoBehaviour
     {
         if (collision2D.gameObject.tag == "Pavement")
         {
-            Debug.Log("Player has entered a Pavement block");
+            //Hides previous command text on new block entered
+            CommandText.color = new Color32(255, 255, 255, 0);
+            CommandText.outlineWidth = 0.2f;
+            CommandText.outlineColor = new Color32(0, 0, 0, 0);
+
+            //Debug.Log("Player has entered a Pavement block");
 
             BlockText.color = Pink;
             BlockText.outlineWidth = 0.2f;
@@ -62,7 +91,12 @@ public class Dialogue : MonoBehaviour
 
         if (collision2D.gameObject.tag == "Road")
         {
-            Debug.Log("Player has entered a Road block");
+            //Hides previous command text on new block entered
+            CommandText.color = new Color32(255, 255, 255, 0);
+            CommandText.outlineWidth = 0.2f;
+            CommandText.outlineColor = new Color32(0, 0, 0, 0);
+
+            //Debug.Log("Player has entered a Road block");
 
             BlockText.color = Orange;
             BlockText.outlineWidth = 0.2f;
@@ -73,7 +107,12 @@ public class Dialogue : MonoBehaviour
 
         if (collision2D.gameObject.tag == "Stairs")
         {
-            Debug.Log("Player has entered a Stair block");
+            //Hides previous command text on new block entered
+            CommandText.color = new Color32(255, 255, 255, 0);
+            CommandText.outlineWidth = 0.2f;
+            CommandText.outlineColor = new Color32(0, 0, 0, 0);
+
+            //Debug.Log("Player has entered a Stair block");
 
             BlockText.color = Green;
             BlockText.outlineWidth = 0.2f;
@@ -84,7 +123,12 @@ public class Dialogue : MonoBehaviour
 
         if (collision2D.gameObject.tag == "Bollard")
         {
-            Debug.Log("Player has entered a Bollard block");
+            //Hides previous command text on new block entered
+            CommandText.color = new Color32(255, 255, 255, 0);
+            CommandText.outlineWidth = 0.2f;
+            CommandText.outlineColor = new Color32(0, 0, 0, 0);
+
+            //Debug.Log("Player has entered a Bollard block");
 
             BlockText.color = Blue;
             BlockText.outlineWidth = 0.2f;
@@ -95,7 +139,12 @@ public class Dialogue : MonoBehaviour
 
         if (collision2D.gameObject.tag == "TrafficLights")
         {
-            Debug.Log("Player has entered a Traffic Light block");
+            //Hides previous command text on new block entered
+            CommandText.color = new Color32(255, 255, 255, 0);
+            CommandText.outlineWidth = 0.2f;
+            CommandText.outlineColor = new Color32(0, 0, 0, 0);
+
+            //Debug.Log("Player has entered a Traffic Light block");
 
             BlockText.color = Purple;
             BlockText.outlineWidth = 0.2f;
@@ -112,16 +161,19 @@ public class Dialogue : MonoBehaviour
         {
             if (Random.Range(1, 3) == 1) //1 or 2
             {
-                Debug.Log("Print Go (Pavement)");
-                CommandText.color = new Color32(255, 255, 255, 255);
+                //Debug.Log("Print Go (Pavement)");
+                CommandText.color = Pink;
                 CommandText.outlineWidth = 0.2f;
                 CommandText.outlineColor = new Color32(0, 0, 0, 255);
                 
                 Secs = Random.Range(1, 5); //1 to 5
-                Trigger = true; //RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-                Debug.Log("raaahahhhhfdhfsbjhsabfjhd");
+                Debug.Log(Secs);
+                Trigger = true; //Sets the timer to count down
+                Debug.Log("Timer Started");
+                CommandText.SetText("Wait");
 
-                CommandText.SetText("Go");
+                PavementBool = true;
+
             }
             else if (Random.Range(1, 3) == 2) // 1 or 2
             {
@@ -137,53 +189,65 @@ public class Dialogue : MonoBehaviour
         if (collision2D.gameObject.tag == "RoadCol")
         {
             Debug.Log("Stop (Road/Bollard)");
-            CommandText.color = new Color32(255, 255, 255, 255);
+            CommandText.color = Orange;
             CommandText.outlineWidth = 0.2f;
             CommandText.outlineColor = new Color32(0, 0, 0, 255);
 
-            CommandText.SetText("Stop");
+            Secs = Random.Range(1, 5); //1 to 5
+            Trigger = true; //Sets the timer to count down
+            Debug.Log("Timer Started");
+            CommandText.SetText("Wait");
+
+            RoadBool = true;
         }
-        else
+        /*else
         {
             Debug.Log("Disabled (Road/Bollard)");
             CommandText.color = new Color32(255, 255, 255, 0); //Opacity 0
             CommandText.outlineColor = new Color32(0, 0, 0, 0); //Opacity 0
-        }
+        }*/
 
 
         //TRAFFIC LIGHTS - Stop, Wait (for light sprite to change), Cross
         if (collision2D.gameObject.tag == "TLCol")
         {
             Debug.Log("Stop (Traffic Lights)");
-            CommandText.color = new Color32(255, 255, 255, 255);
+            CommandText.color = Purple;
             CommandText.outlineWidth = 0.2f;
             CommandText.outlineColor = new Color32(0, 0, 0, 255);
 
-            CommandText.SetText("Wait");
+            //CommandText.SetText("Wait"); //Needs different code to wait for the animation to finish
+
+            //TLBool = true;
         }
-        else
+        /*else
         {
             Debug.Log("Disabled (Traffic Lights)");
             CommandText.color = new Color32(255, 255, 255, 0); //Opacity 0
             CommandText.outlineColor = new Color32(0, 0, 0, 0); //Opacity 0
-        }
+        }*/
 
 
         //STAIRS - Stop, Stairs
         if (collision2D.gameObject.tag == "StairsCol")
         {
             Debug.Log("Stop (Stairs)");
-            CommandText.color = new Color32(255, 255, 255, 255);
+            CommandText.color = Green;
             CommandText.outlineWidth = 0.2f;
             CommandText.outlineColor = new Color32(0, 0, 0, 255);
 
+            Secs = 3; //Stairs will alwasys need a 3 second wait
+            Trigger = true; //Sets the timer to count down
+            Debug.Log("Timer Started");
             CommandText.SetText("Stop");
+
+            StairsBool = true;
         }
-        else
+        /*else
         {
             Debug.Log("Disabled (Stairs)");
             CommandText.color = new Color32(255, 255, 255, 0); //Opacity 0
             CommandText.outlineColor = new Color32(0, 0, 0, 0); //Opacity 0
-        }
+        }*/
     }
 }
